5e8d7917c112601ed5466beda7a7380503f4e9c2
wawacode <291217806@qq.com>
2021-08-27 14:32:17 +0800

    vue demo second stage

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,24 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-
+import Recommand from '@/views/recommand'
+import Singer from '@/views/singer'
+import TopList from "@/views/top-list"
+import Search from "@/views/search"
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/recommand',
+    component: Recommand
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path:'/singer',
+    component:Singer
+  },
+  {
+    path:'/top-list',
+    component:TopList
+  },
+  {
+    path:'/search',
+    component:Search
   }
 ]
 
---
+++ src/views/recommand.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class='recommand'>
+        推荐页面
+    </div>
+</template>
+<script>
+    export default{
+        name:"recommand"
+    }
+</script>
+<style lang='scss' scoped>
+</style>s
